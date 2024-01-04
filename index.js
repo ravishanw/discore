bed5aba1940f6b5e99a4f49cc641a752e6f16047
Ravishan Wijemanne <ravishansays@gmail.com>
2024-01-03 19:17:38 -0500

    exposing port as an env variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
