9da38eb4073c89a97751372d305141a15b4d1e51
Ravishan Wijemanne <ravishansays@gmail.com>
2024-05-23 07:34:44 -0400

    added floating rocket back to top button with intersection observer

--- public/js/frontEnd.js
+++ public/js/frontEnd.js
@@ -32,11 +32,11 @@ $(document).ready(()=>{
         $(".tos-container").addClass("fadeIn");
         setTimeout(()=>{
             $(".tos-container").removeClass("fadeIn");
-        }, "1500");
+        }, 1500);
         $(".privacy-container").addClass("fadeIn");
         setTimeout(()=>{
             $(".privacy-container").removeClass("fadeIn");
-        }, "1500");
+        }, 1500);
     });
     
     function cursorAppear() {
@@ -48,8 +48,32 @@ $(document).ready(()=>{
         $(".link-container a").removeClass("concealer");
         $(".link-container a").addClass("fadeIn");
     }
-    setTimeout(cursorAppear,"500");
-    setTimeout(fadeFx,"2900");
+    setTimeout(cursorAppear,500);
+    setTimeout(fadeFx,2900);
+
+    // Intersection observer
+
+    let options = {
+        rootMargin: "0px",
+        threshold: 1.0,
+        }
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) {
+                $(".floating-rocket").css("visibility", "visible");
+                $(".floating-rocket").addClass("fadeIn");
+                setTimeout(() => {
+                    $(".floating-rocket").removeClass("fadeIn");
+                }, 1500);
+            } else {
+                $(".floating-rocket").css("visibility", "hidden");
+            }
+        });
+    }, options);
+
+    const scrollDivs = document.querySelector(".scroll-div");
+    observer.observe(scrollDivs);
     
 
 });
